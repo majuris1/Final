089d490fe2f30768c26356eac4f7847b6961c805
Unknown <majuris1@hawkmail.newpaltz.edu>
2017-05-15 12:11:22 -0400

    Added Pipe

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	public string s = Slide; 
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		var player = coll.gameObject.GetComponent<Player>();
+		coll.GetComponent<Collider2D>();
+		if (gameObject.GetComponent<coll>)
+			StartCoroutine("Slide");
+	}
+	IEnumerator Slide() 
+	{
+		if(Input.GetKeyDown("s"))
+			GetComponent<Collider2D>().SetActive(false);
+	}
+
+}
