c34848274dcc4af541286abd51647091283da7c7
Unknown <majuris1@hawkmail.newpaltz.edu>
2017-05-15 09:18:34 -0400

    Added Levels 2 and 3

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Fruit : MonoBehaviour {
 
-	public int points = 1;
+	public int lives = 1;
 
 	private void OnTriggerEnter2D(Collider2D coll)
 	{
 		var player = coll.gameObject.GetComponent<Player>();
 		if(player != null) {
 			gameObject.SetActive(false);
-			FindObjectOfType<GM>().SetPoints(FindObjectOfType<GM>().GetPoints() + 1);
+			FindObjectOfType<GM>().SetLives(FindObjectOfType<GM>().GetLives() + 1);
 		}
     }
-
 }
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Throwable{
+
+	public float blastRadius = 5;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		var player = coll.gameObject.GetComponent<Player>();
+		if (isActive && player == null) {
+			Explode();
+		}
+	}
+
+	public void Explode()
+	{
+		// Get a reference to all enemies
+		var enemies = FindObjectsOfType<Enemy>();
+
+		// Loop through each enemy in the list
+		foreach (var e in enemies) {
+
+			// Check if that enemy is within the blast radius
+			if (Vector3.Distance(this.transform.position, e.transform.position) < blastRadius) {
+
+				// Set that enemy to NOT-Active
+				e.gameObject.SetActive(false);
+			}
+		}
+
+		//Set myself (aka the bomb) to NOT-Active. That way the bomb disappears, and can not be picked up again.
+		gameObject.SetActive(false);
+
+	}
+	void Update()
+	{
+		transform.Rotate(0, 0, 50*Time.deltaTime);
+	}
+}
